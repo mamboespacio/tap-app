027623f05d9e8cd218009d7f617c8f7d4be74e8a
mamboespacio <emadobao+1@gmail.com>
2025-01-09 10:33:02 -0300

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx,jsx,ts,tsx,css}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = ['./src/**/*.{js,jsx,jsx,ts,tsx,css}'];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
